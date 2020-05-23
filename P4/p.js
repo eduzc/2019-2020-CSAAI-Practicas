2d04dbbc9f64bd870fd954db395765372db54dc7
Edu <ej.zeballos.2017@urjc.alumnos.es>
2020-05-23 21:34:53 +0200

    css terminado y empezando funcionalidad

---
+++ P4/p.js
@@ -0,0 +1,17 @@
+function main()
+{
+  console.log("Reproductor de video")
+
+  //-- Obtenemos el elemento correspondiente de cada video
+  video1 = document.getElementById("video1")
+  video2 = document.getElementById("video2")
+  video3 = document.getElementById("video3")
+  screen_ = document.getElementById("pantalla")
+
+  //-- Funcionalidad con botones
+  watch1 = document.getElementById('play1')
+  watch2 = document.getElementById('play2')
+  watch3 = document.getElementById('play3')
+
+  //-- Una vez hacemos click en ver video
+  //-- Indicamos la fuente de este
