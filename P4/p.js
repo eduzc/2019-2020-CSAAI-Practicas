628c834a05b2d2258cc0b1a60330cbab803d7fea
Edu <ej.zeballos.2017@urjc.alumnos.es>
2020-05-24 00:56:52 +0200

    funcionalidad

--- P4/p.js
+++ P4/p.js
@@ -15,3 +15,50 @@ function main()
 
   //-- Una vez hacemos click en ver video
   //-- Indicamos la fuente de este
+
+  watch1.onclick = () => {
+    console.log("Reproduciendo...")
+    screen_.src = "Video_1.mp4";
+    screen_.currentTime = video1.currentTime;
+  }
+  watch2.onclick = () => {
+    console.log("Reproduciendo...")
+    screen_.src = "Video_2.mp4";
+    screen_.currentTime = video2.currentTime;
+  }
+  watch3.onclick = () => {
+    console.log("Reproduciendo...")
+    screen_.src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
+    screen_.currentTime = video3.currentTime;
+  }
+
+  //-- Muestra si hemos hecho click en el video
+
+  video1.onmouseover = () => {
+    console.log("Mouse over video1!!")
+    console.log("Musica video1!!")
+    video1.muted = false;
+  }
+  video1.onmouseout = () => {
+    console.log("Mouse out!!")
+    video1.muted = true;
+  }
+  video2.onmouseover = () => {
+    console.log("Mouse over video2!!")
+    console.log("Musica video2!!")
+    video2.muted = false;
+  }
+  video2.onmouseout = () => {
+    console.log("Mouse out!!")
+    video2.muted = true;
+  }
+  video3.onmouseover = () => {
+    console.log("Mouse over video3!!")
+    console.log("Musica video3!!")
+    video3.muted = false;
+  }
+  video3.onmouseout = () => {
+    console.log("Mouse out!!")
+    video3.muted = true;
+  }
+}
